eeebd188f02f6f24a4ebabaa262e947edbbae172
Yuvaraja Subramaniam <subramaniam.yuvaraja@gmail.com>
2020-04-14 14:09:27 +0530

    Added explanation for insertion sort

--- Sorting/002_InsertionSort.cpp
+++ Sorting/002_InsertionSort.cpp
@@ -16,20 +16,20 @@ void insertion_sort(int dt[], int sz);
 
 int main()
 {
-cout << "Insertion Sort Algorithm (Inplace Sort)" << endl;
-cout << "---------------------------------------" << endl;
-
-int data[]     = {99, 5, 77, 50, 58, 23, 88, 11};
-int data_size  = 8;
-
-cout << "Unsorted Array : " << endl;
-display(data, data_size);
-
-cout << "Sorting Array : " << endl;
-insertion_sort(data, data_size);
-
-cout << "Sorted Array : " << endl;
-display(data, data_size);
+	cout << "Insertion Sort Algorithm (Inplace Sort)" << endl;
+	cout << "---------------------------------------" << endl;
+	
+	int data[]     = {99, 5, 77, 50, 58, 23, 88, 11};
+	int data_size  = 8;
+	
+	cout << "Unsorted Array : " << endl;
+	display(data, data_size);
+	
+	cout << "Sorting Array : " << endl;
+	insertion_sort(data, data_size);
+	
+	cout << "Sorted Array : " << endl;
+	display(data, data_size);
 }
 
 
@@ -38,6 +38,14 @@ display(data, data_size);
 //Average case 	- O(n squared)
 //Worst case	- O(n squared)
 //Space complexity - O(1)
+
+//Insertion sort is an inplace sorting algorithm that works by partitioning the dataset into two parts - a sorted part and and unsorted part
+//The initial size of the sorted part is one (i.e just the first element). The algorithm picks one element from the unsorted part at a time
+//and finds the right place for the item to insert into the sorted array. This way the sorted list keeps expanding and the unsorted list keeps
+//shrinking. The algorithm stops when all the items from the unsorted array has been inserted into the sorted array. The algorithm finds the 
+//right place for each unsorted element by shoving the items towards the right in the sorted list as long as the items in the sorted list are
+//greater than the unsorted element that needs to be inserted into the sorted list.
+
 void insertion_sort(int dt[], int sz) {
 	for( int i=1; i < sz; i++) {
 
